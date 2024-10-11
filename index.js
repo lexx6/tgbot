efed312f14788c2cf87ff23b6743998e73ed82c6
lexx696 <lexx696@yandex.ru>
2024-10-11 11:46:07 +0300

    add ? command

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-import { Bot } from "grammy";
+import { Bot, InputFile } from "grammy";
 
 const bot = new Bot("7188438211:AAHPx0EHqN_UOHWZor8KG2N2Geb3fV979_I");
 
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
+const init = async () => {
 
-// Handle the /start command.
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+    // You can now register listeners on your bot object `bot`.
+    // grammY will call the listeners when users send messages to your bot.
 
-// Handle other messages.
-bot.on("message", (ctx) => {
-    if (ctx.message.from.id !== 934288492) {
-        ctx.reply(`Доступ запрещен`)
-        return;
-    }
-    // if (ctx.chat.id !== 934288492) ctx.reply(`Доступ запрещен`)
-    const allowed = ['+', '-', '*', '/', '(', ')', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    const msg = ctx.message.text.split('').filter((el) => allowed.includes(el)).join('')
-    try {
-        const result = eval(msg)
-        ctx.reply(`${result}`)
-    } catch {
-        ctx.reply(`Неправильное выражение`)
-    }
-});
+    bot.api.setMyCommands([
+        { command: "start", description: "Запускает бот" },
+    ]);
 
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
+    // Handle the /start command.
+    bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
-// Start the bot.
+
+    // Handle other messages.
+    bot.hears(/=$/, async (ctx) => {
+        if (ctx.message.from.id !== 934288492) {
+            await ctx.reply(`Доступ запрещен`)
+            return;
+        }
+        // if (ctx.chat.id !== 934288492) ctx.reply(`Доступ запрещен`)
+        const allowed = ['+', '-', '*', '/', '(', ')', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        const msg = ctx.message.text?.split('').filter((el) => allowed.includes(el)).join('')
+        try {
+            const result = eval(msg)
+            await ctx.reply(`${result}`)
+        } catch {
+            await ctx.reply(`Неправильное выражение`)
+        }
+    });
+
+    bot.hears(/\?$/, async (ctx) => {
+        const randomNum = Math.round(Math.random())
+        if (!!randomNum) {
+            await ctx.replyWithPhoto(new InputFile("./Yes.png"));
+        } else {
+            await ctx.replyWithPhoto(new InputFile("./No.png"));
+        }
+    })
+
+    // Now that you specified how to handle messages, you can start your bot.
+    // This will connect to the Telegram servers and wait for messages.
+
+    // Start the bot.
+}
+
 bot.start();
+init();
 
